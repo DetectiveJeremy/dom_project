820e38d888b26ebb5a0f6005783f9d4f72054405
DetectiveJeremy <jeremy.r.herrera.ortiz@gmail.com>
2023-08-01 11:48:58 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Add your code to this file
+// Single pokemon images
+
+let pokemonImg = document.getElementsByClassName("pokemon_images");
+// Looping each img
+for (i = 0; i < pokemonImg.length; i++) {
+    pokemonImg[i].style.border = "2px dotted orange"
+}
+// Setting background
+let body = document.querySelector("body")
+body.style.backgroundColor = "#082f77"
+// Setting "Let's learn about our gen1 starters" to center
+let h2Gen1 = document.querySelector("h2")
+h2Gen1.style.textAlign = "center"
